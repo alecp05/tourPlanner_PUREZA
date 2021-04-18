0995636f6758c9d14eeeadf93eea4463f61b7c1f
Alexander Pureza <alecpureza@yahoo.de>
2021-04-18 15:53:45 +0200

    PostgresConnection established

--- src/dataaccesslayer/database.java
+++ src/dataaccesslayer/database.java
@@ -3,27 +3,55 @@ package dataaccesslayer;
 import models.logModel;
 import models.tourModel;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class database implements dataAccess{
 
+    private String url;
+    private String user;
+    private String password;
     private String connectionString;
 
     public database(){
         //get info from config file
         connectionString = "...";
         //establish DB connection
+
+        url ="jdbc:postgresql://localhost:5432/tourPlanner";
+        user = "postgres";
+        password = "alecUser";
     };
 
     @Override
     public List<tourModel> GetTourItems() {
+
+        //int count = 0;
+        ResultSet myRs = null;
+        try(Connection connection = DriverManager.getConnection(url, user,password);
+            PreparedStatement statement2 = connection.prepareStatement("SELECT * FROM touritems")
+        ){
+            myRs = statement2.executeQuery();
+
+            while(myRs.next()) {
+
+                String name = myRs.getString(1);
+                System.out.println(name);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
         tourModel[] tourModels = {
                 new tourModel("Tour 1", "Vienna to Lower Austria",170 ,"Vienna","Lower Austria"),
                 new tourModel("Tour 2", "Vienna to Upper Austria",  220,"Vienna","Upper Austria"),
                 new tourModel("Tour 3", "Vienna to Salzburg",  330,"Vienna","Salzburg")
         };
+
+
         return new ArrayList<tourModel>(Arrays.asList(tourModels));
     }
 
