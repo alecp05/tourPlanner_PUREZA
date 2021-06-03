7b3878210eab6af380b7551a74a921cecbe95a7c
Alexander Pureza <alecpureza@yahoo.de>
2021-06-03 19:22:22 +0200

    filesystem all tested

--- test/fileTests/images.java
+++ test/fileTests/images.java
@@ -1,22 +1,31 @@
 package fileTests;
 
 import dataaccesslayer.file.filesystem;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
 public class images {
 
+    private filesystem filesystem;
+
+    @Before
+    public void setup(){
+        filesystem = new filesystem();
+    }
+
     @Test
     public void testSavingImageInFile_shouldBeSaved() throws IOException {
         //System.out.println("testing images");
-        filesystem filesystem = new filesystem();
         try{
             filesystem.SaveImageInFile("https://i.pinimg.com/originals/ab/ce/cc/abcecc889ad3c5705166f8edf3efe1a6.jpg"
                     ,"testingImage01");
@@ -28,7 +37,7 @@ public class images {
         int checkCount = 0;
         if(exists){
             checkCount = 1;
-            System.out.println("check");
+            //System.out.println("check");
         }
 
         assertTrue("The Image is not being saved properly",checkCount == 1);
@@ -43,7 +52,7 @@ public class images {
         try {
             filesystem.SaveImageInFile("---","testingImage02");
         }catch (Exception e){
-            System.out.println("not working");
+            //System.out.println("not working");
             checkNumber = 1;
         }
 
@@ -52,4 +61,75 @@ public class images {
     }
 
 
+    @Test
+    public void testDeletingImageInFile_shouldBeDeleted(){
+        try{
+            filesystem.SaveImageInFile("https://i.pinimg.com/originals/ab/ce/cc/abcecc889ad3c5705166f8edf3efe1a6.jpg"
+                    ,"testingImage02");
+        }catch (Exception e){
+            System.out.println("Could not convert Url into a Picture");
+        }
+
+        int checkNumber = 0;
+        try{
+            filesystem.DeleteImage("testingImage02");
+            checkNumber = 1;
+        }catch (Exception e){
+            System.out.println("Could not convert Url into a Picture");
+        }
+
+        assertTrue("The Image could not be deleted", checkNumber == 1);
+
+    }
+
+    @Test
+    public void testSaveImageInGallery_shouldBeSaved() throws IOException {
+        BufferedImage img = ImageIO.read(new File("C:/Users/alecp/OneDrive/Bilder/bild_1.jpeg"));
+        //String pathForImage = selectedFile.getName();
+        int checkCount = 0;
+        try {
+            filesystem.saveImageForGallery(img,"bild_01.jpeg");
+        }catch (Exception e){
+            System.out.println("Image could not be saved in Gallery");
+        }
+
+        File tmpDir = new File("./galleryImages/"+"bild_01.jpeg");
+        boolean exists = tmpDir.exists();
+        if(exists){
+            checkCount = 1;
+            //System.out.println("check");
+        }
+        assertTrue("Image could not be saved in Gallery", checkCount == 1);
+    }
+
+    @Test
+    public void testGalleryNames_shouldNotBeNull(){
+        List<String> allGalleryNames = new ArrayList<>();
+        allGalleryNames = filesystem.getGalleryNames();
+
+        int checkCount = 0;
+        if(allGalleryNames != null){
+            checkCount = 1;
+        }
+        assertTrue( "All ImagesNames are not correctly generated", checkCount == 1);
+    }
+
+    @Test
+    public void testDeletingGalleryImage_shouldBeDeleted() throws IOException {
+        BufferedImage img = ImageIO.read(new File("C:/Users/alecp/OneDrive/Bilder/bild_1.jpeg"));
+        int checkCount = 0;
+        try {
+            filesystem.saveImageForGallery(img,"bild_02.jpeg");
+        }catch (Exception e){
+            System.out.println("Image could not be saved in Gallery");
+        }
+        try{
+            filesystem.deleteImageFromGallery("bild_02.jpeg");
+            checkCount = 1;
+        }catch (Exception e){
+            System.out.println("Could not convert Url into a Picture");
+        }
+
+        assertTrue("The Image could not be deleted", checkCount == 1);
+    }
 }
